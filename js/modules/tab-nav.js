a56dc7dce3d698c9eb9effc3944dde38d993b705
André R. de Melo <andrermelo1@hotmail.com>
2021-08-20 16:54:56 -0300

    Transformando tab-nav em classe

--- js/modules/tab-nav.js
+++ js/modules/tab-nav.js
@@ -1,21 +1,30 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll('[data-tabMenu = js-tabmenu] li');
-  const tabContent = document.querySelectorAll('[data-tabContent="js-tabcontent"] section');
-
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo');
-    });
-    tabContent[index].classList.add('ativo');
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = 'ativo';
   }
 
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
-
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
-        activeTab(index);
-      });
+  // Ativa a tab de acordo com o index da mesma
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass);
     });
+    this.tabContent[index].classList.add(this.activeClass);
+  }
+
+  // Adiciona os eventos nas tabs
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => this.activeTab(index));
+    });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      // Ativar primeiro item
+      this.activeTab(0);
+      this.addTabNavEvent();
+    }
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion-list.js';
-import initTabNav from './modules/tab-nav.js';
+import TabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
@@ -16,7 +16,9 @@ scrollSuave.init();
 const accordion = new Accordion('.js-accordion dt');
 accordion.init();
 
-initTabNav();
+const tabNav = new TabNav('[data-tabMenu=js-tabmenu] li', '[data-tabContent="js-tabcontent"] section');
+tabNav.init();
+
 initModal();
 initTooltip();
 initDropdownMenu();
