bd97f1d74cd2e1817578eebaf87cead4af8bf39e
André R. de Melo <andrermelo1@hotmail.com>
2021-08-20 16:24:28 -0300

    Accordion transformado em classe

--- js/modules/accordion-list.js
+++ js/modules/accordion-list.js
@@ -1,17 +1,30 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll('.js-accordion dt');
-  const activeClass = 'ativo';
-  function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = 'ativo';
+  }
+  // const accordionList = document.querySelectorAll('.js-accordion dt');
+
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
   }
 
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
-
-    accordionList.forEach((item) => {
-      item.addEventListener('click', activeAccordion);
+  // Adiciona os eventos ao accordion.
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener('click', () => {
+        this.toggleAccordion(item);
+      });
     });
+  }
+
+  // Inicia a função
+  init() {
+    if (this.accordionList.length) {
+      // Ativa o primeiro item do accordion
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
   }
 }
