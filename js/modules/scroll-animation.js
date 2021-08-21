faa628ab9bd3eba4a70e5ab59f25c9757afd5a76
André R. de Melo <andrermelo1@hotmail.com>
2021-08-21 14:58:44 -0300

    Scroll animatio refatorado

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -3,25 +3,45 @@ export default class ScrollAnimation {
     this.sections = document.querySelectorAll(sections);
     this.halfOfWindow = window.innerHeight * 0.6;
 
-    this.showElementsOnScroll = this.showElementsOnScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  showElementsOnScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - this.halfOfWindow) < 0;
+  // Pega a distância de cada item em relação ao topo do site.
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.halfOfWindow),
+      };
+    });
+  }
 
-      if (isSectionVisible) {
-        section.classList.add('ativo');
-      } else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
+  // Verifica a distância em cada objeto
+  // em relação ao scroll do site.
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
+        item.element.classList.remove('ativo');
       }
+      console.log(item);
     });
   }
 
   init() {
-    this.showElementsOnScroll();
-    window.addEventListener('scroll', this.showElementsOnScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
+  }
+
+  // Remove o listener que ativa a animação
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
     return this;
   }
 }
